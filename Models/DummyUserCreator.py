c38568dc54685ca8bcb09dab99d10843138bb217
Michael Shea <mjshea3@illinois.edu>
2018-02-24 14:47:28 -0600

    added some categories

--- Models/DummyUserCreator.py
+++ Models/DummyUserCreator.py
@@ -1,2 +1,24 @@
+import numpy as np
+
+
+nameSize =
+emailSize =
+passwordSize =
+CategorySize =
+Categories = np.array(["Animal.txt", "Artifact.txt",
+                      "Computer.txt", "Food.txt", "Fungus.txt",
+                      "Geo.txt", "Person.txt", "Plant.txt",
+                      "Sport.txt"])
+
+SpCat
+
+def createURLS():
+    list = np.array([])
+
 def createUser():
-    name = 
+
+class DummyUser:
+    def __init(self)
+        self.name =
+        self.password =
+        self.email =
