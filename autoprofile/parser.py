3de3e28af7086e14bbcb3dc1579fd9d4fd1c09bc
Peter Tatkowski <tatkows2@illinois.edu>
2018-02-24 13:53:02 -0600

    added things to get my db stuff thingy working and twerkin

--- autoprofile/parser.py
+++ autoprofile/parser.py
@@ -1,22 +1,42 @@
-import json
+import json,csv,sqlite3
+from random import choice
+from string import ascii_lowercase
+
+#Import all cities, sort by population
+def getCities(c):
+  with open(c, newline='') as csvfile:
+    citiesReader = csv.DictReader(csvfile)
+    ret = []
+    #TODO: RANK CITIES BY POPULATION
+    for row in citiesReader:
+      ret.append([row['city'],row['state_name']])
+    return ret
 
 #block of code for the first name
-with open('nametable.json') as json_file:
-    json_data = json.load(json_file)
-    
+def getNames(c,k):
+  with open(c, newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    ret = []
+    for row in reader:
+      ret.append(row[k])
+    return ret
 
-#block of code for the last name
+#returns a random string of length l
+def getRandStr(l):
+  return ''.join(choice(ascii_lowercase) for i in range(l))
 
+#Stores each "random" entry into a db.  
+def storeInDb(cities,fNames,lNames):
+  conn = sqlite3.connect('people.db')
+  
+def main():
+  cities = getCities('uscitiesv1.3.csv')
+  fNames = getNames('firstnames.csv','name')
+  lNames = getNames('lastnames.csv','name')
+  gender = getGender('firstnames.csv','gender')
+  rcity = choice(range(len(cities)))
+  rname = choice(range(len(fnames)))
+  storeInDb(cities,fNames,gender,lNames)
 
-#block of code for the location
-
-
-#block of code for the email addr
-
-
-#block of code for the password
-
-
-
-
+main()
 ##fo = open(firstname + " " + lastname + ".txt", "wb+")
