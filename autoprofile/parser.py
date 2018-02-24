8b7b7f90d93a2b11d147306928b0e4fadf199271
August Gress <gress.august@gmail.com>
2018-02-24 12:58:58 -0600

    added parser directory to create profiles

---
+++ autoprofile/parser.py
@@ -0,0 +1,22 @@
+import json
+
+#block of code for the first name
+with open('nametable.json') as json_file:
+    json_data = json.load(json_file)
+    
+
+#block of code for the last name
+
+
+#block of code for the location
+
+
+#block of code for the email addr
+
+
+#block of code for the password
+
+
+
+
+##fo = open(firstname + " " + lastname + ".txt", "wb+")
