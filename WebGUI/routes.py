8b92fda72caf2ac85f26529efa2f7513b41d1739
Bhaivsh Bhattar <bhabhavish@gmail.com>
2018-02-25 01:21:19 -0600

    woo

--- WebGUI/routes.py
+++ WebGUI/routes.py
@@ -1,14 +1,24 @@
+import os
 from flask import Flask, render_template, request, session, redirect, url_for #import class and function
 from models import db, User, Place #import db variable
 from forms import SignupFrom, LoginForm, AddressForm
+from werkzeug.utils import secure_filename
+
+
+UPLOAD_FOLDER = 'pics/'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
 app = Flask(__name__) #creates instance of flask class
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://localhost/hackillinois2018' #used to attach database to application look at flasksqlalchemy page for more info
-
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 db.init_app(app) #initialize app to use this setup
 
 app.secret_key = "development-key" #protects against XSS
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route("/") #map this slash to the function index
 def index():
@@ -62,11 +72,38 @@ def login():
             user = User.query.filter_by(email=email).first()
             if user is not None and user.check_password(password):
                 session['email'] = form.email.data #sets email for session object
-                return redirect(url_for('budget'))
+                return redirect(url_for('home'))
             else:
-                return redirect(url_for('index'))
+                return redirect(url_for('upload'))
     elif request.method == 'GET':
         return render_template('login.html', form=form)
+
+@app.route('/upload', methods = ["GET","POST"])
+def upload():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('budget'))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    '''
 
 @app.route("/logout") #pops session which is same as logging out
 def logout():
